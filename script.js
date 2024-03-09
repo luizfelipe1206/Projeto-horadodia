03aec69278c9a6bdbf2241e09693f2ba32a4db6a
Luiz Felipe <luizdantas490@yahoo.com>
2024-03-09 17:36:25 -0300

    atualizando erros de navegador

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg');
     var img = document.getElementById('foto');
     var gradient;
-    
+
     function atualizarHora() {
         var data = new Date();
         var hours = data.getHours();
@@ -13,34 +13,25 @@ function carregar() {
         if (hours >= 6 && hours < 12) {
             //BOM DIA
             img.src = '/fotos/Dia.jpg';
-            gradient = 'background: -webkit-linear-gradient(left, #3C9CEC, #283F5B); ' +
-                       'background: -moz-linear-gradient(left, #3C9CEC, #283F5B); ' +
-                       'background: -ms-linear-gradient(left, #3C9CEC, #283F5B); ' +
-                       'background: -o-linear-gradient(left, #3C9CEC, #283F5B); ' +
-                       'background: linear-gradient(to right, #3C9CEC, #283F5B);';
+            gradient = 'linear-gradient(to right, #3C9CEC, #283F5B)';
         } else if (hours >= 12 && hours <= 17) {
             //BOA TARDE
             img.src = '/fotos/tarde.jpg';
-            gradient = 'background: -webkit-linear-gradient(left, #FA7906, #5B3C28); ' +
-                       'background: -moz-linear-gradient(left, #FA7906, #5B3C28); ' +
-                       'background: -ms-linear-gradient(left, #FA7906, #5B3C28); ' +
-                       'background: -o-linear-gradient(left, #FA7906, #5B3C28); ' +
-                       'background: linear-gradient(to right, #FA7906, #5B3C28);';
+            gradient = 'linear-gradient(to right, #FA7906, #5B3C28)';
         } else {
             //BOA NOITE
             img.src = '/fotos/noite.jpg';
-            gradient = 'background: -webkit-linear-gradient(left, #DC4804, #000000); ' +
-                       'background: -moz-linear-gradient(left, #DC4804, #000000); ' +
-                       'background: -ms-linear-gradient(left, #DC4804, #000000); ' +
-                       'background: -o-linear-gradient(left, #DC4804, #000000); ' +
-                       'background: linear-gradient(to right, #DC4804, #000000);';
+            gradient = 'linear-gradient(to right, #DC4804, #000000)';
         }
 
-        document.body.setAttribute('style', gradient);
+        document.body.style.backgroundImage = gradient;
     }
 
     // Atualizar a hora a cada segundo
     setInterval(atualizarHora, 1000);
 }
+
+// Chame a função carregar após o carregamento do documento
+document.addEventListener('DOMContentLoaded', carregar);
 
 
